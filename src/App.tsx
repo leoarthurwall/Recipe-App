a6492f2c950d9562c2a9d90aa2d9df932df1ac20
leoarthurwall <leowall93@gmail.com>
2022-09-26 13:42:37 +0100

    cleared file ready for project. need batter internet connection firstly.

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,17 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault()
-  }
 
   useEffect(() => {
-    console.log("Effect has been run");
   },[]);
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
+      <form >
         <input type="text" />
         <button type="submit">Go</button>
       </form>
-      <h1 onClick={handleClick}> {count}</h1>
     </div>
   );
 };
