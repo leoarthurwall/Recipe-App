3b54c553c875489db52236a7cf5050d4fd297804
leoarthurwall <leowall93@gmail.com>
2022-09-27 13:05:21 +0100

    gave the apiURL an external variable and added the variable to the fetch requerst to make the code cleaner

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,14 @@ const App: React.FC = () => {
   const API_KEY = process.env.REACT_APP_RECIPE_API_KEY
   const API_ID = process.env.REACT_APP_RECIPE_API_ID
 
-  // const apiRequest =  `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
+  const apiRequest =  `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
 
   useEffect(() => {
     getRecipes()
   }, []);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
-    );
+    const response = await fetch(apiRequest);
     const data = await response.json();
     console.log(data)
   }
