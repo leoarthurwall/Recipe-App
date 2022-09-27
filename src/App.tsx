ba8198183d7708342f5bacdcca33c562a9260319
leoarthurwall <leowall93@gmail.com>
2022-09-27 13:37:29 +0100

    added query template literal to the url and as a parameter to the fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,23 @@ import "./App.css";
 import Recipe from "./components/Recipe";
 
 const App: React.FC = () => {
-  const [ingredient, setIngredient] = useState<string>("");
-  const [recipes, setRecipes] = useState([])
+  // const [ingredient, setIngredient] = useState<string>("");
+  const [recipesFound, setRecipesfound] = useState([])
 
   const API_KEY = process.env.REACT_APP_RECIPE_API_KEY
   const API_ID = process.env.REACT_APP_RECIPE_API_ID
 
-  const apiRequest =  `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
+  const apiRequest =  `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}`
 
   useEffect(() => {
     getRecipes()
   }, []);
 
-  const getRecipes = async () => {
+  const getRecipes = async (query: string) => {
     const response = await fetch(apiRequest);
     const data = await response.json();
-    setRecipes(data.hits)
+    setRecipesfound(data.hits)
+    console.log(data.hits)
   }
 
 
@@ -39,8 +40,7 @@ const App: React.FC = () => {
         </label>
         <input type="submit" value="Search"></input>
       </form>
-      <h3>{ingredient}</h3>
-      <Recipe />
+      <Recipe recipes={recipes} setRecipes={setRecipes}/>
     </div>
   );
 };
