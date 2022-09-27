07a4d1217af2ece1b4ff117a66f3c75fc56c0fa8
leoarthurwall <leowall93@gmail.com>
2022-09-27 11:30:43 +0100

    added an onchange handler and store the input in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
+  const[ingredient, setIngredient] = useState<string>("")
+
   useEffect(() => {}, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIngredient(e.target.value)
+    console.log(ingredient)
+  }
 
   return (
     <div className="App">
-      <form>
+      <form >
         <label>
           Search for an ingredient: 
-          <input type="text"></input>
+          <input onChange={handleChange} type="text"></input>
         </label>
 
         <input type="submit" value="Search"></input>
