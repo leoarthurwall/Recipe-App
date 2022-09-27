2d1046bda76d9f8acaba15361058c0f2f3e35c39
leoarthurwall <leowall93@gmail.com>
2022-09-27 13:02:31 +0100

    added an async/await fetch request to the api and then put it inside a use affect so the data console.log is called upon page render

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,34 @@ const App: React.FC = () => {
   const API_KEY = process.env.REACT_APP_RECIPE_API_KEY
   const API_ID = process.env.REACT_APP_RECIPE_API_ID
 
-  // const Request =  `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
+  // const apiRequest =  `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getRecipes()
+  }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    setIngredient(e.target.value);
-    console.log(ingredient);
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data)
+  }
 
-  };
+
+
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault()
+  //   setIngredient(e.target.value);
+  //   console.log(ingredient);
+
+  // };
 
   return (
     <div className="App">
       <form>
         <label>
           Search for an ingredient:
-          <input onChange={handleChange} type="text"></input>
+          <input type="text"></input>
         </label>
         <input type="submit" value="Search"></input>
       </form>
