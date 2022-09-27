a72504831ad7d3a41592190a804ffe860f67a4dc
leoarthurwall <leowall93@gmail.com>
2022-09-27 23:07:52 +0100

    added an on change event handler and saved the input to a state

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./App.css";
 import Recipe from "./components/Recipe";
 
 const App: React.FC = () => {
-  // const [ingredient, setIngredient] = useState<string>("");
+  const [submitIngredient, setSubmitIngredient] = useState<string>("");
+  const [writtenIngredient, setWrittenIngredient] = useState<string>("");
   const [recipesFound, setRecipesfound] = useState([]);
-  const [query, setQuery] = useState('mushroom')
+  const [query, setQuery] = useState("mushroom");
 
   const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
   const API_ID = process.env.REACT_APP_RECIPE_API_ID;
@@ -13,30 +14,34 @@ const App: React.FC = () => {
   const apiRequest = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}`;
 
   useEffect(() => {
-    getRecipes(query);
+    getRecipes();
   }, []);
 
-  const getRecipes = async (query: string): Promise<any> => {
+  const getRecipes = async (): Promise<any> => {
     const response = await fetch(apiRequest);
     const data = await response.json();
     setRecipesfound(data.hits);
     console.log(data.hits);
   };
 
-  
-  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   e.preventDefault()
-  //   setIngredient(e.target.value);
-  //   console.log(ingredient);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event.target);
+    // setSubmitIngredient(e.target.value);
+    // console.log(ingredient);
+  };
 
-  // };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setWrittenIngredient(event.target.value)
+  };
 
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Search for an ingredient:
-          <input type="text"></input>
+          <input onChange={handleChange} type="text"></input>
         </label>
         <input type="submit" value="Search"></input>
       </form>
