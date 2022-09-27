24285da90ee6d19069591eadc72eece5a2da4525
leoarthurwall <leowall93@gmail.com>
2022-09-27 12:35:33 +0100

    got the recipe api key & id. put them in a .env file and the .env in the gitignore file

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,32 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  const[ingredient, setIngredient] = useState<string>("")
+  const [ingredient, setIngredient] = useState<string>("");
+
+  const API_KEY = process.env.REACT_APP_RECIPE_API_KEY
+  const API_ID = process.env.REACT_APP_RECIPE_API_ID
+
+  // const Request =  `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
 
   useEffect(() => {}, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIngredient(e.target.value)
-    console.log(ingredient)
-  }
+    e.preventDefault()
+    setIngredient(e.target.value);
+    console.log(ingredient);
+
+  };
 
   return (
     <div className="App">
-      <form >
+      <form>
         <label>
-          Search for an ingredient: 
+          Search for an ingredient:
           <input onChange={handleChange} type="text"></input>
         </label>
-
         <input type="submit" value="Search"></input>
       </form>
+      <h3>{ingredient}</h3>
     </div>
   );
 };
