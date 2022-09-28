23daa9f74dbbb1ea9a0925576aaf8af7a2932725
leoarthurwall <leowall93@gmail.com>
2022-09-28 23:42:44 +0100

    added styling to the time and yield icons

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,6 +1,6 @@
 import { IRecipe } from "../IRecipe";
 import "./Recipe.css";
-
+import { ClockIcon, UserIcon } from "@heroicons/react/24/solid";
 type Props = {
   recipe: IRecipe;
 };
@@ -23,25 +23,21 @@ const Recipe: React.FC<Props> = ({ recipe }) => {
             <h1 className="recipe-name">{recipe.recipe.label}</h1>
           </a>
           <h3 className="recipe-ingredient-header">Ingredients:</h3>
-          <ul className="recipe-ingredient-list"> 
+          <ul className="recipe-ingredient-list">
             {recipe.recipe.ingredientLines.map((ingredient: string) => (
               <li className="recipe-ingredient-item">{ingredient}</li>
             ))}
           </ul>
         </div>
         <div className="recipe-right-lower">
-          <p className="recipe-time">
-            Total Time: {recipe.recipe.totalTime} mins
-          </p>
-          {recipe.recipe.yield.length <= 1 ? (
-            <p className="recipe-serving">
-              Serves {recipe.recipe.yield} person
-            </p>
-          ) : (
-            <p className="recipe-serving">
-              Serves {recipe.recipe.yield} people
-            </p>
-          )}
+          <div className="recipe-time">
+            <ClockIcon className="clock-icon" />
+            <p className="clock-icon-text">{recipe.recipe.totalTime}</p>
+          </div>
+          <div className="recipe-yield">
+            <UserIcon className="yield-icon" />
+            <p className="yield-icon-text">{recipe.recipe.yield}</p>
+          </div>
         </div>
       </div>
     </div>
