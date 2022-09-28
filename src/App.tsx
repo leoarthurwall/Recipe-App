7012e0a5d2524891552c4b7bb281c8963c848801
leoarthurwall <leowall93@gmail.com>
2022-09-28 12:14:11 +0100

    adde3d index as the second parameter in the map function and then enclosed the elements in a div with the index key parameter. no more key error

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,19 @@ const App: React.FC = () => {
   const API_ID = process.env.REACT_APP_RECIPE_API_ID;
 
   const apiRequest = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}`;
+  
+  //when the query is updated, the useEffect calls the getRecipes api call function
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
+  // does an async/ await fetch request to the API, adds the data to the recipesFound[] state
+  const getRecipes = async (): Promise<any> => {
+    const response = await fetch(apiRequest);
+    const data = await response.json();
+    setRecipesfound(data.hits);
+    console.log(recipesFound);
+  };
   // saves written input to writtenIngredient state
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
@@ -26,18 +38,6 @@ const App: React.FC = () => {
     console.log(query);
   };
 
-  //when the query is updated, the useEffect calls the getRecipes api call function
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
-
-  // does an async/ await fetch request to the API, adds the data to the recipesFound[] state
-  const getRecipes = async (): Promise<any> => {
-    const response = await fetch(apiRequest);
-    const data = await response.json();
-    setRecipesfound(data.hits);
-    console.log(recipesFound);
-  };
 
   return (
     <div className="App">
@@ -50,11 +50,11 @@ const App: React.FC = () => {
       </form>
       {query && <p>Results for {query}...</p>}
       {recipesFound &&
-        recipesFound.map((recipe) => (
-          <>
+        recipesFound.map((recipe, index) => (
+          <div key={index}>
             <p>{recipe.label}</p>
             <p>hello</p>
-          </>
+          </div>
           // <Recipe key={recipe.calories} recipe={recipe}></Recipe>
         ))}
     </div>
@@ -62,3 +62,6 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+//check state setup, also... follow nonTS tutorial to compare.
+//it seems to be re-rendering every time I add to this commern
