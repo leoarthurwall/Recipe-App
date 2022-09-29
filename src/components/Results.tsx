f537e9948bdc73b3296d5f8a44d31d8792181585
leoarthurwall <leowall93@gmail.com>
2022-09-29 13:12:22 +0100

    added ternary operator that conditionally renders the results button component only when a query is searched

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,29 +1,30 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
-import React from 'react'
-import './Results.css'
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import "./Results.css";
 
 type Props = {
-    query: string
-    setQuery: any // not specific
-}
+  query: string;
+  setQuery: any; // not specific
+};
 
-const Results:React.FC<Props> = ({query, setQuery}) => {
-
-   const handleDelete = () => {
-        setQuery("")
-    }
+const Results: React.FC<Props> = ({ query, setQuery }) => {
+  const handleDelete = () => {
+    setQuery("");
+  };
 
   return (
-    <div className='results-container'>
-         {query && <p className='results-for-text'>Results for... </p>}
-         <button className='results-button'>
-         <XMarkIcon onClick={handleDelete} className='button-icon'/>
-            <p className='button-query'>{query} </p>
+    <>
+      {query && (
+        <div className="results-container">
+          <p className="results-for-text">Results for... </p>
+          <button className="results-button">
+            <XMarkIcon onClick={handleDelete} className="button-icon" />
+            <p className="button-query">{query} </p>
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 
-         </button>
-         
-    </div>
-  )
-}
-
-export default Results
+export default Results;
