46de62c2cc0b7e6e7d8f396c7d8e75b8cd6bd16d
leoarthurwall <leowall93@gmail.com>
2022-09-29 12:40:22 +0100

    styled the results for button. now need to give it functionality

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,6 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
 import React from 'react'
+import './Results.css'
 
 type Props = {
     query: string
@@ -6,8 +8,14 @@ type Props = {
 
 const Results:React.FC<Props> = ({query}) => {
   return (
-    <div>
-         {query && <p>Results for {query}...</p>}
+    <div className='results-container'>
+         {query && <p className='results-for-text'>Results for... </p>}
+         <button className='results-button'>
+         <XMarkIcon  className='button-icon'/>
+            <p className='button-query'>{query} </p>
+
+         </button>
+         
     </div>
   )
 }
