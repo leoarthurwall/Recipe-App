4a1f3327ff24ac481bc4c632b8bb69736f99320b
leoarthurwall <leowall93@gmail.com>
2022-09-30 14:25:43 +0100

    added a ternary opperator for the results that returns random suggestions when you open the app, top results for the ingredient you search for, or nothing if you click x on the ingredient button

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,4 +1,4 @@
-import { XMarkIcon, ListBulletIcon} from "@heroicons/react/24/solid";
+import { XMarkIcon, ListBulletIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import "./Results.css";
 
@@ -14,16 +14,24 @@ const Results: React.FC<Props> = ({ query, setQuery }) => {
 
   return (
     <>
-      {query && (
-        <div className="results-container">
-            <ListBulletIcon className="list-icon"/>
-          <p className="results-for-text">Top Results for... </p>
-          <button className="results-button">
-            <XMarkIcon onClick={handleDelete} className="button-icon" />
-            <p className="button-query">{query} </p>
-          </button>
-        </div>
-      )}
+    { query === "Suggestions" ? (<div className="results-container">
+      <ListBulletIcon className="list-icon" />
+      <p className="results-for-text">Looking for Inspiration? </p>
+      <button className="results-button">
+        <XMarkIcon onClick={handleDelete} className="button-icon" />
+        <p className="button-query">{query} </p>
+      </button>
+    </div>) : query === "" ? (null) : (<div className="results-container">
+      <ListBulletIcon className="list-icon" />
+      <p className="results-for-text">Top Results for... </p>
+      <button className="results-button">
+        <XMarkIcon onClick={handleDelete} className="button-icon" />
+        <p className="button-query">{query} </p>
+      </button>
+    </div>)
+    } 
+    
+   
     </>
   );
 };
